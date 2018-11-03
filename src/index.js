96c02a46233a29e0d2475dd04e92d2053724a924
Darian Gratton <Dariangratton@gmail.com>
2018-11-02 21:37:00 -0700

    added index.js and index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Tank extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+    }
+}
+
+class Turret extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+}
