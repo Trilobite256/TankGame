3aa74c0d17a03edad0b8af9c37f9002cfa165d5c
unknown <dariangratton@gmail.com>
2019-05-18 13:04:24 -0700

    Added BootStrap

--- refactor/index.js
+++ refactor/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 // Import files
 app.use('/css', express.static(__dirname + '/css'));
+app.use('/view', express.static(__dirname + '/view'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
