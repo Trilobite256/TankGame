17cd15bfcae32655d42616e48f28191824d0b046
Darian Gratton <dariangratton@gmail.com>
2019-05-24 18:38:40 -0700

    Cleaned up code for the events

--- refactor/view/IndexView.js
+++ refactor/view/IndexView.js
@@ -9,25 +9,55 @@ var IndexView = function IndexView() {
 IndexView.prototype = {
 
     init: function () {
-        
+        this.createElements()
+            .setupHandlers()
+            .enableEvents();
+    },
+
+    createElements: function () {
+        // Get the document objects
         this.$joinRoomButton = $('#joinRoom');
+        this.$createRoomButton = $('#createRoom');
 
+        return this;
+    },
+
+    setupHandlers: function () {
         this.joinRoomButtonHandler = this.joinRoomButton.bind(this);
+        this.createRoomButtonHandler = this.createRoomButton.bind(this);
         
+        // Handlers from Event Handlers
         this.joinRoomHandler = this.joinRoom.bind(this);
+        this.createRoomHandler = this.createRoom.bind(this);
 
+        return this;
+    },
+
+    enableEvents: function() {
         this.$joinRoomButton.click(this.joinRoomButtonHandler);
+        this.$createRoomButton.click(this.createRoomButtonHandler);
 
+        // Event Handlers
         this.joinRoomEvent.attach(this.joinRoomHandler);
+        this.createRoomEvent.attach(this.createRoomHandler);
 
+        return this;
     },
 
     joinRoomButton: function () {
         this.joinRoomEvent.notify();
     },
 
+    createRoomButton: function () {
+        this.createRoomEvent.notify();
+    },
+
     joinRoom: function () {
-        alert("Test");
+        alert("Join Room");
+    },
+
+    createRoom: function () {
+        alert("Create Room");
     }
 
 }
