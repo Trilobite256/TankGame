62de247d932997fd2ae865283b32ede7c277e6d2
Darian Gratton <dariangratton@gmail.com>
2019-05-26 22:43:02 -0700

    Implemented the logic for index page's controller

--- refactor/controller/IndexController.js
+++ refactor/controller/IndexController.js
@@ -4,3 +4,36 @@ var IndexController = function (model, view) {
 
     this.init();
 }
+
+IndexController.prototype = {
+
+    init: function () {
+        this.setupHandlers()
+            .enableEvents();
+    },
+
+    setupHandlers: function () {
+        
+        this.joinRoomHandler = this.joinRoom.bind(this);
+        this.createRoomHandler = this.createRoom.bind(this);
+
+        return this;
+    },
+
+    enableEvents: function () {
+        
+        this.view.joinRoomEvent.attach(this.joinRoomHandler);
+        this.view.createRoomEvent.attach(this.createRoomHandler);
+
+        return this;
+    },
+
+    joinRoom: function () {
+        console.log($("#roomCodeInput").val());
+    },
+
+    createRoom: function () {
+        console.log("Create Room Button Pressed");
+    }
+
+}
--- refactor/driver/app.js
+++ refactor/driver/app.js
@@ -1,3 +1,4 @@
 $(function () {
     let indexView = new IndexView();
+    let indexController = new IndexController(null, indexView);
 });
